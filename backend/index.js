06ce54ad1eccea70ee024fc3ef513b0ca93ff12c
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-27 00:15:04 +0530

    removed index.js and moved all data to app.js and server.js

--- backend/index.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const socketio = require("socketio");
-const http = require("http");
-
-
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
+const http = require("http");
+const socketio = require("socket.io");
+
 const app=require("./app.js");
+
+const server = http.createServer(app);
+const io = socketio(server);
 
 const PORT = app.get("port");
 const environment = app.get("env");
 
-app.listen(PORT,() => {
+server.listen(PORT,() => {
     console.log(`app listening on port ${PORT}!`)
     console.log(`app is running in ${environment}`);
 });
