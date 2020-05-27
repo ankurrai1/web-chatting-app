5856cad17a22f1eac40cf48889fd1528e1285966
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-28 00:07:19 +0530

    removed current router to simple root get in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+
 dotenv.config({path: ".env"});
-
-
 const app = express();
 
 let port = process.env.PORT;
 app.set('port', port);
 
+app.get('/', function(req, res){ 
+    res.send('<html>all is well...</html>');
+});
 
 module.exports = app;
