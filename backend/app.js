944d535ba984b276265675f31a84885a5f067fd5
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-05-27 23:41:37 +0530

    added dotevn package to for configuring env

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({path: ".env"});
 
 
 const app = express();
 
 let port = process.env.PORT;
-
+console.log(port, ": port");
 app.set('port', port);
 
 
