4b70fd9956747aa3c089459c59bd1e19faaedf3b
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-06-04 23:42:23 +0530

    added onChange setName and setRoom to join

--- client/src/component/Join.js
+++ client/src/component/Join.js
@@ -9,8 +9,8 @@ const Join = ()=>{
     return (
         <div className="joinOuterContainer">
             <div className="joinInnerContainer">
-                <div><input className="joinInput" type="text" placeholder="Name" onChange={}></input></div>
-                <div><input className="joinInput" type="text  mt-20" placeholder="Room" onChange={}></input></div>
+                <div><input className="joinInput" type="text" placeholder="Name" onChange={(event) => setName(event.target.value)}></input></div>
+                <div><input className="joinInput" type="text  mt-20" placeholder="Room" onChange={(event) => setRoom(event.target.value)}></input></div>
                 <Link>
                    <button className="button mt-20" type="submit">Sign in</button>
                 </Link>
