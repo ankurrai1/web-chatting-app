79c257383baf680ea356092a7c8f076f0ae9952f
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-06-05 21:15:33 +0530

    after comming to chat page name and room is set

--- client/src/component/Chat/Chat.js
+++ client/src/component/Chat/Chat.js
@@ -9,9 +9,12 @@ const Chat = ( { location } )=>{
 
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
-    
+
     useEffect(()=>{
         const { name, room } = querystring.parse(location.search);
+        
+        setName(name);
+        setRoom(room);
     })
 
     return (
