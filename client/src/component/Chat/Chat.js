dd1b61b9e99576a39c409f8a5dc652879d3e2775
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-06-05 21:14:09 +0530

    added name and fuction with use state and room too

--- client/src/component/Chat/Chat.js
+++ client/src/component/Chat/Chat.js
@@ -5,7 +5,15 @@ import io from 'socket.io-client';
 
 import './Chat.css';
 
-const Chat = ()=>{
+const Chat = ( { location } )=>{
+
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    
+    useEffect(()=>{
+        const { name, room } = querystring.parse(location.search);
+    })
+
     return (
         <h1>Chat</h1>
     )
