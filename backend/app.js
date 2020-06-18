827c0fadc0141c0a21258cb4f23b63269db361b2
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-06-18 23:56:44 +0530

    required all user functions to app.js

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ const app = express();
 
 let io  = app.io = socketio();
 
+const {addUser, getUser, getUsersInRoom, removeUser}; = require("./src/Models/User");
+
 io.on( "connection", function( socket ) {
     console.log( "A user connected" );
 
@@ -27,6 +29,7 @@ app.set('port', port);
 const user = require('./src/Routers/user');
 
 app.use(user);
+
 
 app.get('/', function(req, res){ 
     res.send('<html>all is well...</html>');
