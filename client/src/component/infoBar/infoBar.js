9cc753fb1eaf86151a7117e6330d3a9f98c34f26
ankurrai1 <rai.ankur0007@gmail.com>
2020-07-11 11:28:31 +0530

    removed type in InfoBar component

--- client/src/component/infoBar/infoBar.js
+++ client/src/component/infoBar/infoBar.js
@@ -5,7 +5,7 @@ import React from 'react';
 import onlineIcon from '../../icons/onlineIcon.png';
 import closeIcon from '../../icons/closeIcon.png';
 
-import './InfoBar.css';
+import './infoBar.css';
 
 const InfoBar = ({ room }) => (
   <div className="infoBar">
