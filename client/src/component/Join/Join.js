0078dd28f1303f8181c141bd7efde6a44be1d005
ankurrai1 <rai.ankur0007@gmail.com>
2020-07-11 11:17:37 +0530

    removed type in join component

--- client/src/component/Join/Join.js
+++ client/src/component/Join/Join.js
@@ -14,7 +14,7 @@ const Join = ()=>{
                 <h1 className="heading"> Join </h1>
                 <div><input className="joinInput" type="text" placeholder="Name" onChange={(event) => setName(event.target.value)}></input></div>
                 <div><input className="joinInput" type="text mt-20" placeholder="Room" onChange={(event) => setRoom(event.target.value)}></input></div>
-                <Link onClick={(even)=>{(!name || !room)? event.preventDefault(): null}} to={`/chat?name=${name}&room=${room}`} >
+                <Link onClick={(event)=>{(!name || !room)? event.preventDefault(): null}} to={`/chat?name=${name}&room=${room}`} >
                    <button className="button mt-20" type="submit">Sign in</button>
                 </Link>
             </div>
